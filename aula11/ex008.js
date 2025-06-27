a8b8e9d34526efdb67a28f041218d27ab12ef3cf
LucasrNegreiro <Lucasrn7@icloud.com>
2025-06-27 10:02:15 -0300

    condições if e else simples e composta

---
+++ aula11/ex008.js
@@ -0,0 +1,13 @@
+let vel = 80
+
+// condição simples 
+
+if(vel >80){
+
+    console.log(`A velocidade do carro é ${vel}km/h Você ultrapassou o limite... MULTADO!!`)
+}
+// }else if(vel >= 40){
+// console.log(`A velocidade é ${vel}km/h Mantenha se dentro do limite de velocidade`)
+
+// }else
+// console.log('Você está abaixo do limite minimo da VIA saia da pista')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+let Pais = 'EUA'
+
+console.log(`Vivendo em ${Pais}`)
+
+if(Pais == 'Brasil'){
+    console.log('Brasileiro')
+}else
+console.log('Estrangeiro')
