0ec1d67381a9268a07119ee663ee4e1f3e4f1eef
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-02 10:54:14 -0300

    Update script.js

--- aula 12 ex/praticando/script.js
+++ aula 12 ex/praticando/script.js
@@ -1,60 +1,60 @@
 
 
- let botao = document.querySelector('input#but')
-     botao.addEventListener('click', clicar)
-     
- 
+let botao = document.querySelector('input#but')
+botao.addEventListener('click', clicar)
 
-function clicar(){
-     
+
+
+function clicar() {
+
     let data = new Date()
     let ano = data.getFullYear()
 
     let inputAno = document.getElementById('txtano')
     let result = document.getElementById('res')
 
-    if(inputAno.value.length == 0 || Number(inputAno.value) > ano){
+    if (inputAno.value.length == 0 || Number(inputAno.value) > ano) {
         alert('Verifique os dados e tente novamente')
-    }else{
+    } else {
         let inputRadio = document.getElementsByName('radsex')
         let idade = ano - Number(inputAno.value)
         let genero = ''
-        
+
 
         let img = document.createElement('img')
         img.setAttribute('id', 'foto')
 
-        if (inputRadio[0].checked){
-            
-            if(idade >=0 && idade < 10){
+        if (inputRadio[0].checked) {
+
+            if (idade >= 0 && idade < 10) {
                 genero = 'Criança'
                 img.setAttribute('src', 'menino.jpg')
-            }else if(idade < 21){
+            } else if (idade < 21) {
                 genero = 'Jovem'
                 img.setAttribute('src', 'jovemM.jpg')
-            }else if (idade < 60){
+            } else if (idade < 60) {
                 genero = 'Adulto'
-                    img.setAttribute('src', 'adulto.jpg')
-            }else{
+                img.setAttribute('src', 'adulto.jpg')
+            } else {
                 genero = 'Idoso'
                 img.setAttribute('src', 'idoso.jpg')
             }
-            
-        }else{
-            if(idade >=0 && idade < 10){
+
+        } else {
+            if (idade >= 0 && idade < 10) {
                 genero = 'Criança'
                 img.setAttribute('src', 'menina.jpg')
-            }else if(idade < 21){
+            } else if (idade < 21) {
                 genero = 'Jovem'
                 img.setAttribute('src', 'jovemF.jpg')
-            }else if (idade < 60){
+            } else if (idade < 60) {
                 genero = 'Adulto'
                 img.setAttribute('src', 'adultoF.jpg')
-            }else{
+            } else {
                 genero = 'Idoso'
                 img.setAttribute('src', 'idosa.jpg')
             }
-            
+
         }
         result.style.textAlign = 'center'
 
@@ -62,6 +62,6 @@ function clicar(){
 
         result.appendChild(img)
     }
-    
-    
+
+
 }
