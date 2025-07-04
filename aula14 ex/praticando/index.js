8d9d7af1d1a271705cc11ffba1493786b9badc19
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-04 08:25:50 -0300

    Update index.js

--- aula14 ex/praticando/index.js
+++ aula14 ex/praticando/index.js
@@ -1,27 +1,25 @@
 function verificar(){
     let ini = document.getElementById('Inicio')
-    let meio = document.getElementById('meio')
+    let fim = document.getElementById('meio')
     let passo = document.getElementById('passo')
     let res = document.getElementById('res')
-    
-    if (ini.value.length == 0 || meio.value.length == 0 || passo.value.length == 0){
-        res.innerHTML = 'Impossivel contar'
 
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert("[ERROR] Faltam Dados")
     }else{
-        res.innerHTML = 'contando...'
-
+        res.innerHTML = 'contando..'
         let i = Number(ini.value)
-        let m = Number(meio.value)
-        let p = Number (passo.value)
-
-        if(i < m){
-            for(c = i; c <= m; c += p ){
-                res.innerHTML += ` ${c}`
+        let f = Number(fim.value)
+        let p = Number(passo.value)  
+        
+        if (i < f){
+            for(c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} `
             }
         }else{
-            for(c = i; c > m; c -= p ){
-                res.innerHTML += ` ${c}`
+            for(c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} `
             }
         }
-    }
+}
 }
