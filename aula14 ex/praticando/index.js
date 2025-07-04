c0a87ed07310e5bb0140a571d93f95c0b452d682
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-04 08:26:15 -0300

    Update index.js

--- aula14 ex/praticando/index.js
+++ aula14 ex/praticando/index.js
@@ -7,7 +7,7 @@ function verificar(){
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         alert("[ERROR] Faltam Dados")
     }else{
-        res.innerHTML = 'contando..'
+        res.innerHTML = 'contando.. <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)  
