9aa3d6e6d53b5f276eb7a424b41d49f55e8ed7fa
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-07 08:27:40 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,2 +1,15 @@
 let num = [5,8,4]
 console.log(num)
+
+num.push(7)
+num.sort()
+console.log(num)
+
+// enquanto i for menor q os elementos do meu vetor ele continuara contando quais os valores dele
+for( i =0; i <= num.length; i++){
+    console.log(num[i])
+}
+
+for(i in num){
+    console.log(num[i])
+}
